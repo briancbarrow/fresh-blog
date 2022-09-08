463c8c54c50c37b4f54e3881d9d8919025c3fa4b
Brian Barrow <briancbarrow@gmail.com>
2022-09-08 12:26:31 -0600

    updated handler for twitter webhook

--- routes/api/twitter.ts
+++ routes/api/twitter.ts
@@ -1,7 +1,9 @@
 import { HandlerContext } from "$fresh/server.ts";
 
-export const handler = (_req: Request, _ctx: HandlerContext): Response => {
-  console.log("request", _req);
-  console.log("ctx", _ctx);
-  return new Response("webhook works");
+export const handler: Handlers<Data> = {
+  async GET(_req: Request, _ctx: HandlerContext): Response {
+    console.log("request", _req);
+    console.log("ctx", _ctx);
+    return new Response("webhook works");
+  },
 };

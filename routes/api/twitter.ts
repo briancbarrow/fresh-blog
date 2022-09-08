b25978d8dea2dcb4f336de5af5e331853d17b22f
Brian Barrow <briancbarrow@gmail.com>
2022-09-08 12:54:20 -0600

    added hmac

--- routes/api/twitter.ts
+++ routes/api/twitter.ts
@@ -1,9 +1,23 @@
 import { HandlerContext } from "$fresh/server.ts";
+// import * as crypto from "https://deno.land/std@0.154.0/crypto/mod.ts";
+import { hmac } from "https://deno.land/x/hmac@v2.0.1/mod.ts";
 
 export const handler: Handlers<Data> = {
-  async GET(_req: Request, _ctx: HandlerContext): Response {
-    console.log("request", _req);
+  async GET(req: Request, _ctx: HandlerContext): Response {
+    // const hmac = crypto
+    //   .createHmac("sha256", Deno.env.get("TW_CONSUMER"))
+    //   .update(req.crc_token)
+    //   .digest("base64");
+    const myhmac = hmac(
+      "sha256",
+      Deno.env.get("TW_CONSUMER"),
+      req.crc_token,
+      "base64",
+      "base64"
+    );
+    console.log("request", myhmac);
+    console.log("crc_token", req.crc_token);
     console.log("ctx", _ctx);
-    return new Response("webhook works");
+    return new Response(myhmac);
   },
 };
